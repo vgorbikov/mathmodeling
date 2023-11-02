be15eb8baec34a7965aafe066b56e9fe64e9affb
Vadim <V.gorb-123456789@yandex.ru>
2023-11-02 21:10:41 +0300

    lab 6 completed

--- lab_6/main.py
+++ lab_6/main.py
@@ -10,6 +10,9 @@ import matplotlib.pyplot as plt
 
 
 def show_graph(f, a, b):
+    """
+    Выводит график функции f на промежутке [a, b]
+    """
     xs = np.arange(a, b, 0.01)
     ys = [f(x) for x in xs]
 
@@ -19,6 +22,9 @@ def show_graph(f, a, b):
 
 
 def trapezoid_method(f, a, b):
+    """
+    Возвращает значение интеграла функции f в пределах от a до b, найденное по методу трапеций
+    """
     xs = np.arange(a, b, abs(b-a)/100)
     ys = [f(x) for x in xs]
     M = max(ys)
@@ -31,7 +37,7 @@ def trapezoid_method(f, a, b):
         hbot = f(a + step*(i+1))
         S += (htop+hbot)*step/2
     
-    return S
+    return S, N
 
 
 
@@ -59,13 +65,52 @@ def m_k_method2(f, a: float, b: float, n: int):
 
 
 def define_sigma(i: list):
+    """
+    Определяет выборочное среднеквадратичное отклонение для выборки i
+    """
     n = len(i)
     i_mid = sum(i)/n
-    sig = np.sqrt(sum([(ii - i_mid)**2 for ii in i ])/n)
+    sig = np.sqrt(sum([(ii - i_mid)**2 for ii in i ])/(n-1))
     return sig
 
 
 
-def define_delta(sigma: float, gamma: float, n: int):
-    2.98*sigma/np.sqrt(n)
+def define_delta_montekarlo(sigma: float, n: int):
+    """
+    Определяет относительную точность для метода монте-карло 
+    по заданному выборочному среднеквадратичному отклонению sigma выборки из n его оценок
+    """
+    return 2.98*sigma/np.sqrt(n)
 
+
+
+
+
+
+print("Дана функция f(x)=cos(x). Найдём интеграл от f(x) на интервале [-1, 1.5]\n")
+print("Вычисленное (вручную) значение: 1.83896597\n")
+
+tr_res = trapezoid_method(np.cos, -1, 1.5)
+print(f'По методу трапеций:             {tr_res[0]}; δ < 1%; N={tr_res[1]}\n')
+
+n_mk1 = 1000
+d1_mk = 100
+while d1_mk > 1:
+    mk_1_res = [m_k_method1(np.cos, -1, 1.5, n_mk1) for i in range(100)]
+    mk_1_sig = define_sigma(mk_1_res)
+    d1_mk = define_delta_montekarlo(mk_1_sig, n_mk1)*100
+    if d1_mk > 1:
+        n_mk1 *= 10
+print(f'По методу Монте-Карло (1 сп.):  {mk_1_res[0]}; δ = {d1_mk}%; σ* = {mk_1_sig}\n')
+
+n_mk2 = 1000
+d2_mk = 100
+while d2_mk > 1:
+    mk_2_res = [m_k_method1(np.cos, -1, 1.5, n_mk2) for i in range(100)]
+    mk_2_sig = define_sigma(mk_2_res)
+    d2_mk = define_delta_montekarlo(mk_2_sig, n_mk2)*100
+    if d2_mk > 1:
+        n_mk2 *= 10
+print(f'По методу Монте-Карло (2 сп.):  {mk_2_res[0]}; δ = {d2_mk}%; σ* = {mk_2_sig}\n')
+
+show_graph(np.cos, -1, 1.5)
